af2dac95a6546fc4d8efa9277715aa5df7a92195
Nema Ngoma <nemangoma275@gmail.com>
2026-09-01 12:26:36 +0200

    Refactor Truck class with enhanced comments and methods
    Updated comments for clarity and improved method parameters.

--- Lab_1_SmartFleet/Truck.java
+++ Lab_1_SmartFleet/Truck.java
@@ -1,41 +1,51 @@
 public class Truck extends Vehicle implements Maintainable {
 
-    private double cargoCapacity;
+  // Stores the truck's cargo capacity
+  private double cargoCapacity;
 
-    public Truck(String vehicleId, String model, double baseRentalRate,
-                 double cargoCapacity) {
+  // Creates a new truck
+  public Truck(String vehicleId, String model, double baseRentalRate,
+             double cargoCapacity) {
 
-        super(vehicleId, model, baseRentalRate);
-        this.cargoCapacity = cargoCapacity;
+    super(vehicleId, model, baseRentalRate);
+    this.cargoCapacity = cargoCapacity;
+  }
+
+  // Gets the cargo capacity
+  public double getCargoCapacity() {
+    return cargoCapacity;
+  }
+
+  // Updates the cargo capacity
+  public void setCargoCapacity(double cargoCapacity) {
+    this.cargoCapacity = cargoCapacity;
+  }
+
+  // Calculates the total rental cost
+  @Override
+  public double calculateRentalCost(int rentalDays) {
+
+    double rentalCost = getBaseRentalRate() * rentalDays;
+
+    // Add extra charge for large cargo
+    if (cargoCapacity > 5) {
+        rentalCost += 20 * rentalDays;
     }
 
-    public double getCargoCapacity() {
-        return cargoCapacity;
-    }
+    return rentalCost;
+  }
 
-    public void setCargoCapacity(double cargoCapacity) {
-        this.cargoCapacity = cargoCapacity;
-    }
+  // Performs truck maintenance
+  @Override
+  public void performMaintenance() {
+    System.out.println(
+            "Performing maintenance on truck " + getVehicleId()
+    );
+  }
 
-    @Override
-    public double calculateRentalCost(int days) {
-
-        double cost = getBaseRentalRate() * days;
-
-        if (cargoCapacity > 5) {
-            cost += 20 * days;
-        }
-
-        return cost;
-    }
-
-    @Override
-    public void performMaintenance() {
-        System.out.println("Performing maintenance on truck " + getVehicleId());
-    }
-
-    @Override
-    public boolean needsService() {
-        return cargoCapacity > 5;
-    }
+  // Checks if the truck needs servicing
+  @Override
+  public boolean needsService() {
+    return cargoCapacity > 5;
+  }
 }
