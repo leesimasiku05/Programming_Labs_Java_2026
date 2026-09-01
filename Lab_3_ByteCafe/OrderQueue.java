6d4293cc64239f7c51a1ef8dfc87127af3372347
Nema Ngoma <nemangoma275@gmail.com>
2026-09-01 18:45:15 +0200

    Update OrderQueue.java

--- Lab_3_ByteCafe/OrderQueue.java
+++ Lab_3_ByteCafe/OrderQueue.java
@@ -3,59 +3,66 @@ import java.util.Queue;
 
 public class OrderQueue {
 
-    private final Queue<String> orders = new LinkedList<>();
-    private final int MAX_CAPACITY = 5;
+  // Stores customer orders
+  private final Queue<String> orders = new LinkedList<>();
 
-    // Producer method
-    public synchronized void placeOrder(String order) {
+  // Maximum number of orders allowed
+  private static final int MAX_CAPACITY = 5;
 
-        try {
-            // Wait if the queue is full
-            while (orders.size() == MAX_CAPACITY) {
-                wait();
-            }
+  // Adds an order to the queue
+  public synchronized void placeOrder(String order) {
 
-            orders.add(order);
-
-            System.out.println(
-                    Thread.currentThread().getName()
-                    + " placed order: " + order
-                    + " | Orders in queue: " + orders.size()
-            );
-
-            // Notify waiting threads
-            notifyAll();
-
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+    try {
+        // Wait while the queue is full
+        while (orders.size() >= MAX_CAPACITY) {
+            wait();
         }
+
+        // Add the new order
+        orders.add(order);
+
+        System.out.println(
+                Thread.currentThread().getName()
+                + " placed order: " + order
+                + " | Orders in queue: " + orders.size()
+        );
+
+        // Notify waiting threads
+        notifyAll();
+
+    } catch (InterruptedException e) {
+        // Restore the interrupted status
+        Thread.currentThread().interrupt();
     }
+  }
 
-    // Consumer method
-    public synchronized String takeOrder() {
+  // Removes and returns the next order
+  public synchronized String takeOrder() {
 
-        try {
-            // Wait if the queue is empty
-            while (orders.isEmpty()) {
-                wait();
-            }
-
-            String order = orders.remove();
-
-            System.out.println(
-                    Thread.currentThread().getName()
-                    + " took order: " + order
-                    + " | Orders in queue: " + orders.size()
-            );
-
-            // Notify waiting threads
-            notifyAll();
-
-            return order;
-
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return null;
+    try {
+        // Wait while there are no orders
+        while (orders.isEmpty()) {
+            wait();
         }
+
+        // Remove the next order
+        String customerOrder = orders.remove();
+
+        System.out.println(
+                Thread.currentThread().getName()
+                + " took order: " + customerOrder
+                + " | Orders in queue: " + orders.size()
+        );
+
+        // Notify waiting threads
+        notifyAll();
+
+        return customerOrder;
+
+    } catch (InterruptedException e) {
+        // Restore the interrupted status
+        Thread.currentThread().interrupt();
+        return null;
     }
+  }
 }
